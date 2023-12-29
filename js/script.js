a038e67b5bd8e2ec5ef00d10d6f62cf33c5014f5
Pascal Muang Khot <pascal@khot.us>
2023-12-28 18:29:02 -0800

    initial commit

--- js/script.js
+++ js/script.js
@@ -1,43 +1,45 @@
 window.AudioContext = window.AudioContext || window.webkitAudioContext || window.mozAudioContext;
+
 let audio    = document.getElementById('audio')
 let ctx      = ""
 let audioSrc = ""
+
+var globalPlayerClicked = "";
+var intervalId;
+var titleP;
+var urlP;
 
 function start(){
     if (!ctx){
         ctx = new AudioContext();
         analyser = ctx.createAnalyser();
         audioSrc = ctx.createMediaElementSource(audio)
-        // we have to connect the MediaElementSource with the analyser 
         audioSrc.connect(analyser)
         analyser.connect(ctx.destination)
     }
-    // we could configure the analyser: e.g. analyser.fftSize (for further infos read the spec)
-    //analyser.fftSize = 64;
-    // frequencyBinCount tells you how many values you'll receive from the analyser
+
     var frequencyData = new Uint8Array(analyser.frequencyBinCount);
 
-    // we're ready to receive some data!
     var canvas = document.getElementById('canvas'),
         cwidth = canvas.width,
         cheight = canvas.height + 100,
-        meterWidth = 10, //width of the meters in the spectrum
-        gap = 2, //gap between meters
+        meterWidth = 10,
+        gap = 2,
         capHeight = 2,
         capStyle = '#fff',
-        meterNum = 800 / (10 + 2), //count of the meters
-        capYPositionArray = []; ////store the vertical position of hte caps for the preivous frame
+        meterNum = 800 / (10 + 2),
+        capYPositionArray = [];
     ctx = canvas.getContext('2d'),
     gradient = ctx.createLinearGradient(0, 0, 0, 300);
     gradient.addColorStop(1, '#44bd32');
     gradient.addColorStop(0.5, '#4cd137');
     gradient.addColorStop(0.25, '#F79F1F');
     gradient.addColorStop(0, '#e84118');
-    // loop
+
     function renderFrame() {
         var array = new Uint8Array(analyser.frequencyBinCount);
         analyser.getByteFrequencyData(array);
-        var step = Math.round(array.length / meterNum); //sample limited data from the total array
+        var step = Math.round(array.length / meterNum);
         ctx.clearRect(0, 0, cwidth, cheight);
         for (var i = 0; i < meterNum; i++) {
             var value = array[i * step];
@@ -45,22 +47,285 @@ function start(){
                 capYPositionArray.push(value);
             };
             ctx.fillStyle = capStyle;
-            //draw the cap, with transition effect
             if (value < capYPositionArray[i]) {
                 ctx.fillRect(i * 12, cheight - (--capYPositionArray[i]), meterWidth, capHeight);
             } else {
                 ctx.fillRect(i * 12, cheight - value, meterWidth, capHeight);
                 capYPositionArray[i] = value;
             };
-            ctx.fillStyle = gradient; //set the filllStyle to gradient for a better look
-            ctx.fillRect(i * 12 /*meterWidth+gap*/ , cheight - value + capHeight, meterWidth, cheight); //the meter
+            ctx.fillStyle = gradient;
+            ctx.fillRect(i * 12, cheight - value + capHeight, meterWidth, cheight);
         }
         requestAnimationFrame(renderFrame);
     }
     renderFrame();
+    changeVisibilityOfNowPlaying('visible');
+
+    intervalId = setInterval(function() {
+        getMetaDataFromStream(urlP)
+            .then(title => changeCardStatusText("<b>Now playing:</b> " + title + " (" + titleP + ")"))
+            .catch(error => console.error(error));
+    }, 15000);
+
     audio.play();
 }
 
 function stop(){
+    changeVisibilityOfNowPlaying('hidden');
+    clearInterval(intervalId);
     audio.pause()
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    const cardFooters = document.querySelectorAll('.card-footer');
+    const cardBodies = document.querySelectorAll('.card-body-player');
+
+    const customPrompt = document.getElementById('customPrompt');
+    
+    const submitBtn = document.getElementById('submitBtn');
+    const cancelBtn = document.getElementById('cancelBtn');
+    const emptyBtn = document.getElementById('emptyBtn');
+
+    const streamTitleInput = document.getElementById('streamTitle');
+    const streamURLInput = document.getElementById('streamURL');
+
+    cardBodies.forEach(function(cardBody) {
+        cardBody.addEventListener('click', function() {
+            var parentDiv = this.parentElement;
+            var parentClass = parentDiv.className;
+    
+            if (parentClass.includes("player")) {
+                var parts = parentClass.split(" ");
+                var CBplayer = parts.find(part => part.includes("player"));
+            }
+
+            if (parentClass.includes("text-bg-secondary")) {                
+            } else {
+                loadData(CBplayer + '-title').then(title => {
+                    titleP = title;
+                    return loadData(CBplayer + '-url');
+                }).then(url => {
+                    urlP = url;
+
+                    var pppDivs = document.querySelectorAll('.ppp');
+
+                    pppDivs.forEach(element => {
+                        var CurrentBgSuc = Array.from(element.classList).find(cls => cls.includes("text-bg-success"));
+
+                        if(CurrentBgSuc) {
+                            element.classList.remove('text-bg-success');
+                            element.classList.add('text-bg-warning');
+                        }
+
+                    });
+    
+                    var playerDiv = document.querySelector('.' + CBplayer);
+                    if (playerDiv) {
+                        playerDiv.classList.remove('text-bg-warning');
+                        playerDiv.classList.add('text-bg-success');
+
+                        var audioPlayer = document.getElementById('audio');
+                        audioPlayer.src = urlP;
+
+                        changeCardStatusText("<b>Now playing:</b> " + titleP + " - " + urlP);
+
+                        // getMetaDataFromStream(urlP)
+                        // .then(title => console.log('Current Title:', title))
+                        // .catch(error => console.error(error));
+
+                        audioPlayer.load();
+                        
+                        start();
+                    } 
+
+                }).catch(error => {
+                    console.error('Error loading data:', error);
+                });
+            }
+        
+        });
+    });
+
+    cardFooters.forEach(function(cardFooter) {
+        cardFooter.addEventListener('click', function() {
+            var parentDiv = this.parentElement;
+            var parentClass = parentDiv.className;
+    
+            globalPlayerClicked = parentClass;
+    
+            customPrompt.style.display = 'flex';
+        });
+    });
+
+    submitBtn.addEventListener('click', function() {
+        if (globalPlayerClicked.includes("player")) {
+            var parts = globalPlayerClicked.split(" ");
+            var Cplayer = parts.find(part => part.includes("player"));
+        }
+
+        const streamTitle = streamTitleInput.value;
+        const streamURL = streamURLInput.value;
+
+        saveData(Cplayer + '-title', streamTitle);
+        saveData(Cplayer + '-url', streamURL);
+
+        var titleSpan = document.querySelector('.' + Cplayer + '-title');
+        if (titleSpan) {
+            titleSpan.innerHTML = streamTitle;
+        }
+
+        var playerDiv = document.querySelector('.' + Cplayer);
+        if (playerDiv) {
+            playerDiv.classList.remove('text-bg-secondary');
+            playerDiv.classList.add('text-bg-warning');
+        }
+
+        customPrompt.style.display = 'none';
+        clearInputs();
+    });
+
+    cancelBtn.addEventListener('click', function() {
+        customPrompt.style.display = 'none';
+        clearInputs();
+    });
+
+    emptyBtn.addEventListener('click', function() {
+        if (globalPlayerClicked.includes("player")) {
+            var parts = globalPlayerClicked.split(" ");
+            var Cplayer = parts.find(part => part.includes("player"));
+        }
+
+        var titleSpan = document.querySelector('.' + Cplayer + '-title');
+        if (titleSpan) {
+            titleSpan.innerHTML = "&nbsp;";
+        }
+
+        var playerDiv = document.querySelector('.' + Cplayer);
+        if (playerDiv) {
+            playerDiv.classList.add('text-bg-secondary');
+            playerDiv.classList.remove('text-bg-success');
+            playerDiv.classList.remove('text-bg-warning');
+        }
+        
+        customPrompt.style.display = 'none';
+        deleteData(Cplayer + '-title');
+        deleteData(Cplayer + '-url');
+        clearInputs();
+    });
+
+    function clearInputs() {
+        streamTitleInput.value = '';
+        streamURLInput.value = '';
+    }
+});
+
+
+document.addEventListener('DOMContentLoaded', async () => {
+    const variablesList = [];
+    const numberOfPlayers = 6;
+    
+    for (let i = 1; i <= numberOfPlayers; i++) {
+        variablesList.push(`player${i}-title`);
+        variablesList.push(`player${i}-url`);
+    }
+
+    
+    
+    for (const key of variablesList) {
+        try {
+            const data = await loadData(key);
+
+            if (typeof data !== 'undefined') {
+                var playerAlone = key.split('-')[0];
+
+                var playerDiv = document.querySelector('.' + playerAlone);
+                if (playerDiv) {
+                    playerDiv.classList.remove('text-bg-secondary');
+                    playerDiv.classList.add('text-bg-warning');
+                }
+
+                if (key.includes("-title")){
+                    var titleSpan = document.querySelector('.' + playerAlone + '-title');
+                    if (titleSpan) {
+                        titleSpan.innerHTML = data;
+                    }
+                }
+            } else {
+            }
+        } catch (error) {
+            console.error(`Error loading ${key}:`, error);
+        }
+    }
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    var audioElement = document.getElementById("audio");
+    var volumeDisplay = document.getElementById("volumeDisplay");
+    var volumeUp = document.getElementById("volumeUp");
+    var volumeDown = document.getElementById("volumeDown");
+
+    var volume = 0.5;
+    audioElement.volume = volume;
+
+    function updateVolume(change) {
+        volume += change;
+        volume = Math.max(0, Math.min(1, volume));
+        audioElement.volume = volume;
+        volumeDisplay.innerHTML = `<small>${Math.round(volume * 100)}%</small>`;
+    }
+
+    volumeUp.addEventListener("click", function() {
+        updateVolume(0.1);
+    });
+
+    volumeDown.addEventListener("click", function() {
+        updateVolume(-0.1);
+    });
+});
+
+function changeCardStatusText(newText) {
+    var cardStatusTextElement = document.querySelector('.card-status-text');
+    if(cardStatusTextElement) {
+        cardStatusTextElement.innerHTML = newText;
+    } else {
+        console.log("Element with class 'card-status-text' not found.");
+    }
+}
+
+function changeVisibilityOfNowPlaying(visibility) {
+    var elements = document.querySelectorAll('.nowplaying');
+    elements.forEach(function(element) {
+        element.style.visibility = visibility;
+    });
+}
+
+async function getMetaDataFromStream(streamUrl) {
+    try {
+        let statusUrl = new URL(streamUrl);
+        statusUrl.pathname = '/status-json.xsl';
+
+        const response = await fetch(statusUrl.href);
+        const data = await response.json();
+
+        if (streamUrl.endsWith('/')) {
+            const firstStreamInfo = data.icestats.source[0];
+            return firstStreamInfo && firstStreamInfo.title ? firstStreamInfo.title : null;
+        } else {
+            const streamPath = new URL(streamUrl).pathname;
+            const streamInfo = data.icestats.source.find(source => {
+                const sourcePath = new URL(source.listenurl).pathname;
+                return sourcePath === streamPath;
+            });
+
+            if (streamInfo && streamInfo.title) {
+                return streamInfo.title;
+            } else {
+                console.log('Specific stream not found or no title available');
+                return "N/A";
+            }
+        }
+    } catch (error) {
+        console.error('Error fetching stream metadata:', error);
+        return "N/A";
+    }
 }
---
+++ main.js
@@ -0,0 +1,83 @@
+// main.js
+
+// Modules to control application life and create native browser window
+const { app, BrowserWindow, ipcMain, globalShortcut } = require('electron');
+const path = require('node:path')
+const Store = require('electron-store');
+const store = new Store();
+
+const createWindow = () => {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: 980,
+    height: 325,
+    title: "Qust",
+    resizable: false,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true,
+      contextIsolation: false,
+      webSecurity: false
+    },
+    autoHideMenuBar: true,
+  })
+
+  // and load the index.html of the app.
+  mainWindow.loadFile('index.html')
+
+  // Open the DevTools.
+  mainWindow.webContents.openDevTools()
+
+  globalShortcut.register('Ctrl+Alt+Insert', () => {
+    mainWindow.webContents.send('play-action')
+  })
+
+  globalShortcut.register('Ctrl+Alt+H', () => {
+    mainWindow.webContents.send('stop-action')
+  })
+
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.whenReady().then(createWindow);
+
+// IPC handlers for storing and retrieving data
+ipcMain.handle('set-data', (event, key, value) => {
+    store.set(key, value);
+});
+
+ipcMain.handle('delete-data', (event, key) => {
+  store.delete(key);
+});
+
+ipcMain.handle('get-data', (event, key) => {
+    return store.get(key);
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
+});
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+app.on('will-quit', () => {
+  globalShortcut.unregisterAll();
+});
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
